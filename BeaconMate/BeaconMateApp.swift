826f6b30f96b4fd043435df7c303e5719443e0eb
Luis David <luis.david.10@hotmail.com>
2025-08-12 13:14:52 +0200

    Initial Commit

---
+++ BeaconMate/BeaconMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconMateApp.swift
+//  BeaconMate
+//
+//  Created by Alex Guerrero on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BeaconMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
